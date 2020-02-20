1dfab441adf27576e013c60f311ed51a8573daa2
AstrexXx <astrex17@abv.bg>
2020-02-20 20:18:07 +0200

    Library Score -> SetUp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,22 @@
 
 struct library
 {
+    library()
+    {
+        // enter the scoring function
+    }
+
     std::vector<unsigned int> books;
     unsigned int daysForSignUp;
     unsigned int booksPerDay;
+
+    int getScore()
+    {
+        return this -> Score;
+    }
+
+private:
+    int Score;
 };
 
 int main() {
