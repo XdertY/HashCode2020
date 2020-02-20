a86904e8aad644e590e93b3057064ca022407602
AstrexXx <astrex17@abv.bg>
2020-02-20 20:14:15 +0200

    Library Setup

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <vector>
+
+struct library
+{
+    std::vector<unsigned int> books;
+    unsigned int daysForSignUp;
+    unsigned int booksPerDay;
+};
 
 int main() {
     std::cout << "Kur!" << std::endl;
