4cf864f9165a307874a84eefea2f4eab02291de9
XdertY <axelgron@gmail.com>
2020-02-20 19:34:07 +0200

    Create Project

---
+++ cmake-build-debug/CMakeFiles/HashCode2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HashCode2020.dir/main.cpp.o"
+  "HashCode2020"
+  "HashCode2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HashCode2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
